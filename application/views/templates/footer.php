0cc5d264b3d72ac7991f22fdb1f47985b23277c8
rizkuy01 <rizkysaputraa2004@gmail.com>
2023-01-23 09:40:14 +0700

    update readme

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -48,6 +48,12 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.13.1/b-2.3.3/b-colvis-2.3.3/b-html5-2.3.3/b-print-2.3.3/datatables.min.js"></script>
 
+
+<!-- Sweet Alert -->
+
+<script src="<?= base_url('aset/'); ?>swal/dist/sweetalert2.all.js"></script>
+<script src="<?= base_url('aset/'); ?>swal/dist/myscript"></script>
+
 <!-- Core plugin JavaScript-->
 <script src="<?= base_url('aset/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
